e08869de267ba44d5c395af9d0f0caa453224c04
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-04-08 19:53:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os 
 
 class config(object):
-  API_ID = 2443790
-  API_HASH = "b00e27babc25c281d2ec18242b5b6760"
-  TOKEN = "1979779416:AAExgRiGv4aO0_7rbKjhOx_oOUXtWvKP9zw"
+  API_ID = 8838171
+  API_HASH = "0587408d4f7d9301f5295840b0f3b494"
+  TOKEN = "5182594480:AAHDgiv3wRIP9FaePW320_veW0hBBKMzELQ"
 
 # NO EDITS 
   PIC = "https://telegra.ph/file/42aa88442d9266468d11a.jpg"
